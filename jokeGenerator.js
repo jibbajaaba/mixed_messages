4ac58d7e338621a563521a216365bafa1c9a4af9
Mark McTague <mmcta275@gmail.com>
2025-07-02 11:17:35 -0400

    added another joke to jokeStore and created function to grab a random joke and return it

--- jokeGenerator.js
+++ jokeGenerator.js
@@ -1,3 +1,11 @@
-const jokes = require('./jokeStore.js')
+const jokes = require("./jokeStore.js");
 
+const randJokes = () => {
+  let randIndex = Math.floor(Math.random() * jokes.length)
+  let randJoke = jokes[randIndex]
+
+  return `${randJoke.setup}\n${randJoke.punchline}`
+};
+
+console.log(randJokes())
 
--- jokeStore.js
+++ jokeStore.js
@@ -3,5 +3,10 @@ module.exports = [
 		type: 'general',
 		setup: 'What do you call a pony with a cough?',
 		punchline: 'A little horse'
+	},
+	{
+		type: 'general',
+		setup: 'How do you make a tissue dance?',
+		punchline: 'You put a little boogie in it.'
 	}
 ]
